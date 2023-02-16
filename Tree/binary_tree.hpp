3b8f9fab5814a0feca7e4f7d5cc18a7ac0d1273e
Leonardo Manuel Grau Vargas <leonardo.grau@outlook.com.pe>
2023-02-16 01:43:01 -0500

    feat: finished binary tree data stucture

--- Tree/binary_tree.hpp
+++ Tree/binary_tree.hpp
@@ -1,19 +1,90 @@
 #ifndef BINARY_TREE_HPP
 #define BINARY_TREE_HPP
 
+#include <iostream>
+using namespace std;
+
 template <class T>
 class BinaryTreeNode
 {
-private:
+public:
   BinaryTreeNode<T> *left;
   BinaryTreeNode<T> *right;
+private:
   T data;
 public:
-  BinaryTreeNode();
+  BinaryTreeNode(T data) {
+    this->data = data;
+    this->left = nullptr;
+    this->right = nullptr;
+  }
+  //get
+  T getData() { return data; }
+  //set
+
 };
 
+template <class T>
 class BinaryTree
 {
+  public:
+  BinaryTreeNode<T> *root;
+  int elementQuantity;
+  BinaryTree() {
+    root = nullptr;
+    elementQuantity = 0;
+  }
+
+  void _addElement(T data, BinaryTreeNode<T>* &currentNode) {
+    if(currentNode == nullptr) {
+      currentNode = new BinaryTreeNode<T>(data);
+      elementQuantity++;
+      return;
+    }else {
+      if(data < currentNode->getData()) {
+        _addElement(data, currentNode->right);
+      }else {
+        _addElement(data, currentNode->left);
+      }
+    }
+  }
+
+  void _inOrder(BinaryTreeNode<T>* &currentNode) {
+    if(currentNode != nullptr) {
+      _inOrder(currentNode->left);
+      cout << currentNode->getData() << " ";
+      _inOrder(currentNode->right);
+    }
+  }
+
+  void inOrder() {
+    _inOrder(root);
+  }
+
+  void _inOrderReverse(BinaryTreeNode<T>* &currentNode) {
+    if(currentNode != nullptr) {
+      _inOrder(currentNode->right);
+      cout << currentNode->getData() << " ";
+      _inOrder(currentNode->left);
+    }
+  }
+
+  void inOrderReverse() {
+    _inOrderReverse(root);
+  }
+
+  void addElement(T data) {
+    if(root == nullptr) {
+      root = new BinaryTreeNode<T>(data);
+      elementQuantity++;
+    } else
+      _addElement(data, root);
+  }
+
+  int getElementQuantity() {
+    return elementQuantity;
+  }
+
 };
 
 #endif
--- Tree/main.cpp
+++ Tree/main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <time.h>
+
+#include "binary_tree.hpp"
 
 using namespace std;
 
+int getRandomNumber(int min, int max) {
+  return min + rand()%(max - min + 1);
+}
+
 int main() {
-  cout << "Hello, world!" << endl;
+  srand(time(0));
+
+  int randNumber = rand();
+
+  cout << getRandomNumber(1, 20) << endl;
+
+  BinaryTree<int> tree = BinaryTree<int>();
+  vector<int> numbers;
+  int random = 0;
+
+  for(int i = 0; i < 20; i++) {
+    random = getRandomNumber(1, 100);
+    numbers.push_back(random);
+    tree.addElement(random);
+  }
+  
+  //
+  for(int i = 0; i < 20; i++) {
+    cout << numbers.at(i) << " ";
+  }
+  cout << endl;
+  tree.inOrder();
+  cout << endl;
+  tree.inOrderReverse();
+
+   cout << "Element quantity: " << tree.elementQuantity << endl;
   return 0;
 }
